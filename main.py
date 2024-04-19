1c92034a5aa6a5583cfbfefb450326f205f1a660
dealtime101 <dealtime101@hotmail.com>
2024-04-19 17:02:29 -0400

    Functions
    Add Functions to flip image

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from PIL import Image
 
+#Create an image via import
 image = Image.open('cat.jpg')
 
+# analyze the image
+print(image.size)
+print(image.filename)
+print(image.format)
+
+# flip the image
+#image = image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+#image = image.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+#image = image.transpose(Image.Transpose.ROTATE_90)
+
+#exercise
+cat_rotated = image.rotate(30)
+cat_rotated.save('cat_rotated.png', 'png')
+
+#show the image
 image.show()
